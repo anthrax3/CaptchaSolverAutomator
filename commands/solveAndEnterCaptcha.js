5b595f20af059d15215c8dc9b381531ce083266f
Roshin <theroshin@gmail.com>
2018-01-19 23:57:40 +0530

    2Captcha Login done

--- commands/solveAndEnterCaptcha.js
+++ commands/solveAndEnterCaptcha.js
@@ -3,40 +3,44 @@ var fs = require('fs');
 const captcha_image_file_path = "/tmp/captcha.png";
 const GOOGLE_VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate?key=" + process.env.API_KEY;
 
-exports.command = function (cssSelector) {
+exports.command = function loop(cssSelector) {
     var self = this;
-    while(true) {
-        self.url(function (result) {
-            // return the current url
-            console.log(result);
-            if (result === 'https://2captcha.com/enterpage') {
-                return true;
-            }
-            else {
-                self.perform(function () {
-                    // Read image from /tmp
-                    var imageFile = fs.readFileSync(captcha_image_file_path);
-                    var encoded = new Buffer(imageFile).toString('base64');
-                    // Send Post data to Google
-                    var response = request('POST', GOOGLE_VISION_API_URL, {
-                        json: {
-                            requests: {
-                                image: {content: encoded},
-                                features: [{type: 'DOCUMENT_TEXT_DETECTION', 'maxResults': 1}]
-                            }
+    self.url(function (result) {
+        if (result.value === 'https://2captcha.com/enterpage') {
+            self.assert.urlEquals('https://2captcha.com/enterpage');
+            console.log("Logged In!");
+            return self;
+        }
+        else {
+            self.perform(function () {
+                // Read image from /tmp
+                var imageFile = fs.readFileSync(captcha_image_file_path);
+                var encoded = new Buffer(imageFile).toString('base64');
+                // Send Post data to Google
+                var response = request('POST', GOOGLE_VISION_API_URL, {
+                    json: {
+                        requests: {
+                            image: {content: encoded},
+                            features: [{type: 'DOCUMENT_TEXT_DETECTION', 'maxResults': 1}]
                         }
-                    });
-                    //console.log(body);
-                    try {
-                        var captcha_text = JSON.parse(response.getBody('utf8'))['responses'][0]['textAnnotations'][0]['description'];
-                        console.log(captcha_text);
-                        self.setValue(cssSelector, captcha_text)
-                    } catch (err) {
-                        console.log("No Captcha text found")
                     }
                 });
-            }
-        });
-    }
+                //console.log(body);
+                try {
+                    var captcha_text = JSON.parse(response.getBody('utf8'))['responses'][0]['textAnnotations'][0]['description'];
+                    console.log('Captcha Text:' + captcha_text);
+                    self.setValue(cssSelector, 'captcha_text')
+                } catch (err) {
+                    console.log("No Captcha text found");
+                }
+            });
+            self.click('#validation_form > button');
+            self.pause(2000);
+            // Retry Again
+            self.setValue('input[type="password"]', process.env.PASSWORD);
+            self.saveImage("#validation_form > img");
+            self.solveAndEnterCaptcha(cssSelector)
+        }
+    });
     return this
 };
--- tests/demo.js
+++ tests/demo.js
@@ -1,14 +1,8 @@
-var fs = require('fs');
-var request = require('sync-request');
-
 if (!process.env.API_KEY)
     throw new Error("API KEY not defined");
 
 if (!process.env.PASSWORD)
     throw new Error("2Captcha Password not defined");
-
-const GOOGLE_VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate?key=" + process.env.API_KEY;
-const captcha_image_file_path = "/tmp/captcha.png";
 
 module.exports = {
     '2Captcha Login': function (browser) {
